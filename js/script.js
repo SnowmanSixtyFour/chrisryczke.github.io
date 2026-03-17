78696590081087cb4b5f5cb423a3ae475181b207
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-03-17 17:39:58 -0400

    JavaScript Clipboard Button

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function copyUrl() {
+  // Website URL
+  let url = "https://www.chrisryzcke.com";
+
+   // Copy the URL
+  navigator.clipboard.writeText(url);
+
+  // Alert Copy
+  alert("Copied this page to the clipboard!");
+} 
