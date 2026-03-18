d7de847000694f6d93c515cea8986c014926d581
SnowmanSixtyFour <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-03-18 15:37:51 -0400

    Navbar Scrolls with Page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let siteHeader = `
+<div id='topnav'>
 <h2 align="center"><a href="https://chrisryczke.com#About">About Me</a> | <a href="https://chrisryczke.com#Projects">Projects</a> | <a href="./socials">Social Media</a> | <a href="https://chrisryczke.com#Contacts">Contacts</a></h2>
+</div>
 `;
 let siteFooter = `
 <footer>
@@ -21,3 +23,15 @@ function copyUrl() {
   // Alert Copy
   alert("Copied this page to the clipboard!");
 }
+
+// Move Navbar with Scroll
+window.onscroll = function() {scrollFunction()};
+var navBar = document.getElementById("topnav");
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+	  navBar.classList.add("topnav-fixed");
+  } else {
+	  navBar.classList.remove("topnav-fixed");
+  }
+} 
