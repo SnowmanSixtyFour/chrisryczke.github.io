74d831151140484c0fb2c363231704c5b89b2650
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-03-17 17:16:19 -0700

    Consistent Header and Footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+let siteHeader = `
+<h2 align="center"><a href="index#About">About Me</a> | <a href="index#Projects">Projects</a> | <a href="./socials">Social Media</a> | <a href="index#Contacts">Contacts</a></h2>
+`;
+let siteFooter = `
+<footer>
+	<p align="center">&copy; 2026 Chris Ryczke</p>
+</footer>
+`;
+
+// Global
+document.querySelector('#siteHeader').insertAdjacentHTML('beforeend', siteHeader);
+document.querySelector('#siteFooter').insertAdjacentHTML('beforeend', siteFooter);
+
 function copyUrl() {
   // Website URL
   let url = "https://www.chrisryzcke.com";
@@ -7,4 +20,4 @@ function copyUrl() {
 
   // Alert Copy
   alert("Copied this page to the clipboard!");
-} 
+}
