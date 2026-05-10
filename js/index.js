fa9966a9d54edd5de6f0fdca6174c48efcf4b5d5
Snowman64 <162073017+SnowmanSixtyFour@users.noreply.github.com>
2026-05-10 11:54:53 -0400

    Added Option Functionality
    Created About Me, Added Enter Key Functionality with Options

--- js/index.js
+++ js/index.js
@@ -13,8 +13,18 @@ C   C H   H R  R   I      S       R  R    Y   C   C  Z    K  K  E
  CCC  H   H R   R III SSSS        R   R   Y    CCC  ZZZZZ K   K EEEEE`;
 
 let helpText =
-`  F1=Help         F2=Split         F3=Exit                  F7=Backward      F8=Forward
- F10=Actions     F12=Cancel                                                            `;
+` F1=Help         F2=Split         F3=Exit               F7=Backward        F8=Forward
+F10=Actions     F12=Cancel`;
+
+let pageHome =
+`Software Engineer`;
+let page0 =
+`I have been fascinated by technology ever since I was a child.
+
+With experience in object-oriented programming and web development, along with being fast at keyboard typing, I am a skilled software engineer.
+Specializing in both front-end and back-end development.
+
+I am still building my skills, currently studying IBM Mainframe and the COBOL language, using the IBM 3270 terminal.`
 
 // --- DOCUMENT ---
 
@@ -23,8 +33,8 @@ let helpText =
 document.body.style.userSelect = 'none'; // Disable Text Selecting
 
 // Website Size
-document.body.style.width = "57%"; // Width
-document.body.style.scale = "1.7"; // Scale
+document.body.style.width = "50%"; // Width
+document.body.style.scale = "1.8"; // Scale
 
 // Fit Elements on Screen
 document.body.style.margin = "auto"; // Margin
@@ -43,11 +53,11 @@ document.body.style.color = "white"; // Text Colour
 
 document.body.style.textAlign = "center"; // Align Text to Center
 
-document.body.style.whiteSpace = "pre"; // Allow Multiple Lines in Text
+document.body.style.whiteSpace = "pre-wrap"; // Allow Multiple Lines in Text
 
 // Initialize Objects
 
-// Above Header
+// Above Header (Fake Options)
 var menu = document.createElement('p');
 menu.innerHTML = menuText;
 menu.style.color = "cornflowerblue";
@@ -55,6 +65,7 @@ menu.style.color = "cornflowerblue";
 // Header
 var header = document.createElement('p');
 header.innerHTML = headerText;
+header.style.whiteSpace = "pre"; // Prevent Breaking when Window Resized
 
 // Separator
 var separator = document.createElement('div');
@@ -62,8 +73,12 @@ var separator = document.createElement('div');
 separator.style.height = '1px';
 separator.style.backgroundColor = 'cornflowerblue';
 
-// Choices
+// Page
 
+var currentPage = document.createElement('p'); // Page Text
+currentPage.innerHTML = pageHome;
+
+// Choices
 var choices = document.createElement('p');
 choices.innerHTML =
 `0 <span class=choice>About Me</span>
@@ -83,14 +98,15 @@ var optionPrefix = document.createElement('span');
 optionPrefix.innerHTML = "Option ===> ";
 
 var option = document.createElement('input');
-option.type = "text";
-option.id = "option";
-option.style.borderBottom = "1px solid cyan"; // Line
-option.style.width = "100%"; // Set Width to Size
+option.style.width = "100%";
 
+// Help (Fake Keybinds)
 var help = document.createElement('p');
 help.innerHTML = helpText;
 help.style.color = "cornflowerblue";
+
+help.style.display = 'flex'; // Center Text
+help.style.textAlign = 'left'; // Align to Left
 
 // Copyright
 var copyright = document.createElement('p');
@@ -104,6 +120,7 @@ document.body.appendChild(separator);
 document.body.appendChild(header);
 document.body.appendChild(separator.cloneNode(true));
 
+document.body.appendChild(currentPage);
 document.body.append(choices);
 
 document.body.appendChild(optionDiv);
@@ -114,3 +131,15 @@ optionDiv.appendChild(option);
 document.body.appendChild(help);
 
 document.body.appendChild(copyright);
+
+// Events
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        // Go to Selected Page
+        if (option.value === "0") currentPage.innerHTML = page0;
+        if (option.value === "1") currentPage.innerHTML = page1;
+        if (option.value === "2") currentPage.innerHTML = page2;
+        if (option.value === "3") currentPage.innerHTML = page3;
+    }
+})
